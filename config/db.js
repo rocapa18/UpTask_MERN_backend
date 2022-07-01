3636bc1586c19e91fd7f12ef5b7563078c50d139
rocapa18 <roberto.pavano@gmail.com>
2022-07-01 17:26:39 -0300

    último cambio

--- config/db.js
+++ config/db.js
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
 
-const conectarDB = async () => {
+async function conectarDB() {
     try {
-        const connection=await mongoose.connect(process.env.MONGO_URI, {
+        const connection = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        
-        const url =`${connection.connection.host}:${connection.connection.port}`;
-        console.log(`MongoDB conectado en: ${url}`)
+
+        const url = `${connection.connection.host}:${connection.connection.port}`;
+        console.log(`MongoDB conectado en: ${url}`);
 
     } catch (error) {
         console.log(`error: ${error.message}`);
         process.exit(1);
     }
-};
+}
 
 export default conectarDB;
